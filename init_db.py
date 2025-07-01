41b3c7943f4ab392aa19b833fb38dea5c5deb19f
beerhunters <113686882+beerhunters@users.noreply.github.com>
2025-07-01 16:59:28 +0300

    ...

--- init_db.py
+++ init_db.py
@@ -1,17 +1,3 @@
-# from sqlalchemy.ext.asyncio import create_async_engine
-# from models.models import Base
-# import asyncio
-#
-#
-# async def init_db():
-#     engine = create_async_engine("postgresql+psycopg://user:password@db:5432/monitor")
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#     await engine.dispose()
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(init_db())
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import sessionmaker
